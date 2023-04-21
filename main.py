847d342ed987f77bb56f1f6c1e95ebf90c73ca73
Trevor Blanarik <tblanarik@github.com>
2023-04-21 02:09:32 +0000

    adds message_manager, sends flow value

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
 import argparse
-from sms_manager import SMSManager
+from message_manager import MessageManager
 from wsdot_trafficflow import WSDOTTrafficFlow
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Retrieve traffic flow data from WSDOT API and send text message using Twilio API')
+        description='Retrieve traffic flow data from WSDOT API and send message to http endpoint')
     parser.add_argument('--access-code', type=str,
                         required=True, help='WSDOT API access code')
     parser.add_argument('--flow-data-id', type=int, required=True,
                         help='ID of traffic flow data to retrieve')
-    parser.add_argument('--twilio-account-sid', type=str,
-                        required=True, help='Twilio account SID')
-    parser.add_argument('--twilio-auth-token', type=str,
-                        required=True, help='Twilio auth token')
-    parser.add_argument('--to-phone-number', type=str, required=True,
-                        help='Phone number to send traffic flow data to')
-    parser.add_argument('--from-phone-number', type=str,
-                        required=True, help='Phone number to send from')
+    parser.add_argument('--recipients', type=str, required=True,
+                        help='Email recipients separated by commas')
+    parser.add_argument('--post-url', type=str, required=True,
+                        help='URL to send message to')
     args = parser.parse_args()
 
     traffic_flow = WSDOTTrafficFlow(args.access_code, args.flow_data_id)
-    sms = SMSManager(args.twilio_account_sid,
-                     args.twilio_auth_token, args.to_phone_number, args.from_phone_number)
+    message_manager = MessageManager(args.post_url)
 
     flow_data = traffic_flow.flow_data()
     if flow_data.flow_reading_value > 1:
-        sms.send_text_message(
-            f"{flow_data.flow_station_location.description} Slow Traffic")
+        message_manager.send_message(
+            f"{flow_data.flow_station_location.description} Slow Traffic = ({flow_data.flow_reading_value})!",
+            args.recipients)
     else:
         print("No slow traffic: ", flow_data.flow_station_location.description)
